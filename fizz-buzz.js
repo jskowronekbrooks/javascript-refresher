70791a270c9c7055dbe96d7aebc74f4c73154dc8
James Skowronek <jskowronek@brooksschool.org>
2026-03-03 11:50:33 -0500

    Made semicolons consistent

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,24 +1,24 @@
 let numRangeLow = 1;
 let numRangeHigh = 1000;
-let numFizz = 0
-let numBuzz = 0
-let numFizzBuzz = 0
+let numFizz = 0;
+let numBuzz = 0;
+let numFizzBuzz = 0;
 
 
 for (let i = numRangeLow; i < numRangeHigh; i++) {
-    let fizz = 'fizz'
-    let buzz = 'buzz'
+    let fizz = 'fizz';
+    let buzz = 'buzz';
     if (i % 3 && i % 5) {
-        console.log(i + ' ' + fizz + ' ' + buzz)
+        console.log(i + ' ' + fizz + ' ' + buzz);
         numFizzBuzz += 1;
     }
     else if (i % 3) {
-        console.log(i + ' ' + fizz)
+        console.log(i + ' ' + fizz);
         numFizz += 1;
     } else if (i % 5) {
-        console.log(i + ' ' + buzz)
-        numBuzz += 1
+        console.log(i + ' ' + buzz);
+        numBuzz += 1;
     }
 }
 
-console.log('There were ' + numFizz + ' fizz\'s, ' + numBuzz + ' buzz\'s, and ' + numFizzBuzz + ' fizz buzz\'s out of ' + numRangeHigh)
+console.log('There were ' + numFizz + ' fizz\'s, ' + numBuzz + ' buzz\'s, and ' + numFizzBuzz + ' fizz buzz\'s out of ' + numRangeHigh);
