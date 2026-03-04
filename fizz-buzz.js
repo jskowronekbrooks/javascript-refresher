435717a7a8eed66678b54ed0474fbd6bb90fcb84
James Skowronek <jskowronek@brooksschool.org>
2026-03-03 22:54:30 -0500

    Made the conditionals actually check for the correct remainer | printed i if no conditionals passed

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -3,22 +3,22 @@ let numRangeHigh = 1000;
 let numFizz = 0;
 let numBuzz = 0;
 let numFizzBuzz = 0;
+let fizz = 'fizz';
+let buzz = 'buzz';
 
-
-for (let i = numRangeLow; i < numRangeHigh; i++) {
-    let fizz = 'fizz';
-    let buzz = 'buzz';
-    if (i % 3 && i % 5) {
+for (let i = numRangeLow; i <= numRangeHigh; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
         console.log(i + ' ' + fizz + ' ' + buzz);
         numFizzBuzz += 1;
-    }
-    else if (i % 3) {
+    } else if (i % 3 == 0) {
         console.log(i + ' ' + fizz);
         numFizz += 1;
-    } else if (i % 5) {
+    } else if (i % 5 == 0) {
         console.log(i + ' ' + buzz);
         numBuzz += 1;
+    } else {
+        console.log(i);
     }
 }
 
-console.log('There were ' + numFizz + ' fizz\'s, ' + numBuzz + ' buzz\'s, and ' + numFizzBuzz + ' fizz buzz\'s out of ' + numRangeHigh);
+console.log('There were ' + numFizz + ' fizz\'s, ' + numBuzz + ' buzz\'s, and ' + numFizzBuzz + ' fizz buzz\'s out of ' + (numRangeHigh - numRangeLow));
